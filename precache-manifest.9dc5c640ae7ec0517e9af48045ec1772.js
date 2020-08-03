b5d7987ba0130d97c23424b48c2f009bdaa9fc20
nidhiraj98 <nidhiraj9899@gmail.com>
2020-08-03 13:22:21 +0530

    single-page

---
+++ precache-manifest.9dc5c640ae7ec0517e9af48045ec1772.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5615979b7c871aa41afc571454e9e14b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a250bdd1f4c22d5f57bb",
+    "url": "/static/css/main.2bc8ef4a.chunk.css"
+  },
+  {
+    "revision": "22804eae7bd99768ffe8",
+    "url": "/static/js/2.4d09e90d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4d09e90d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a250bdd1f4c22d5f57bb",
+    "url": "/static/js/main.3f1ae93f.chunk.js"
+  },
+  {
+    "revision": "fd06b4173599be0f39b7",
+    "url": "/static/js/runtime-main.f2dff503.js"
+  },
+  {
+    "revision": "44c61fd8928dc5e68417571c0e6cc53e",
+    "url": "/static/media/background.44c61fd8.jpg"
+  }
+]);
